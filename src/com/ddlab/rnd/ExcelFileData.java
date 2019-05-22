98e898697985ac59acf2198a18e9b5a6bf5a690b
debjava <deba.java@gmail.com>
2019-05-22 22:16:31 +0530

    Code committed and pushed by deba.java@gmail.com on 22-May-2019 10:16 PM using DDLAB Gitpusher tool

---
+++ src/com/ddlab/rnd/ExcelFileData.java
@@ -0,0 +1,22 @@
+package com.ddlab.rnd;
+
+public class ExcelFileData {
+  private String fileFormat;
+  private String filePath;
+
+  public String getFileFormat() {
+    return fileFormat;
+  }
+
+  public void setFileFormat(String fileFormat) {
+    this.fileFormat = fileFormat;
+  }
+
+  public String getFilePath() {
+    return filePath;
+  }
+
+  public void setFilePath(String filePath) {
+    this.filePath = filePath;
+  }
+}
